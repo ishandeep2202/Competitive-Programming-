4106a7ee8716a661015a21d1f815184396ecf5ef
Ishandeep Singh <f20240564@pilani.bits-pilani.ac.in>
2025-07-14 23:29:11 +0530

    Update max_distinct_elements.cpp

--- Sliding Window/max_distinct_elements.cpp
+++ Sliding Window/max_distinct_elements.cpp
@@ -1,13 +1,30 @@
-int function(const vector<int>& arr, int k) {
-    int maxDistinct = 0;
-
-    for (int i = 0; i <= arr.size() - k; ++i) {
+int function(vector<int>& arr, int k) {
+    int maxD = 0;
+    for (int i=0; i<=arr.size()-k; i++) {
         set<int> s;
-        for (int j = i; j < i + k; ++j) {
+        for (int j=i; j<i+k; j++) {
             s.insert(arr[j]);
-        }
-        maxDistinct = max(maxDistinct, (int)s.size());
+        maxD = max(maxD, s.size());
     }
+    return maxD;
+}
 
-    return maxDistinct;
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vector <int> a(n);
+    for (int i=0; i<n; i++) 
+        cin >> a[i];
+    map <int, int> mp;
+    for (int i=0; i<k; i++) 
+        mp[a[i]]++;
+    int ans = mp.size();
+    for (int i=k; i<n; i++) {
+        mp[a[i]]++;
+        mp[a[i-k]]--;
+        if (mp[a[i-k]] == 0) 
+            mp.erase(a[i-k]);
+        ans = max(ans mp.size());
+    }
+    cout << ans << endl;
 }
